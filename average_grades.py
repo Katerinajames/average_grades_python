c1a12b4028a5090ab2d202b86b84ae0dff3b47e4
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2024-09-18 15:04:48 +0300

    Add files via upload

--- average_grades.py
+++ average_grades.py
@@ -1,13 +1,18 @@
 
 total=0
-gradecounter=1 
-
-while gradecounter <=5:
-	grade=int(input("Enter grade\n")) 
+gradecounter=0 
+grade=int(input("Enter grade\n")) 
+	
+while grade!=-1:	
 	total=total+grade
 	gradecounter=gradecounter+1
+	grade=int(input("Enter grade\n")) 
+	
+     
+if gradecounter==0:
+  print("No grades were entered")
+else:
+  
+  average=total/gradecounter
 
-
-average=total/5
-
-print("The class average  is %.2f "%(average))
+  print("The class average  is %.2f "%(average))
